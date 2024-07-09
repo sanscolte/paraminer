c00a8cd1cec6ed8c3e457585245695b416a0e195
Timofey <timofeykropotov@gmail.com>
2024-07-09 17:16:11 +0300

    add convert to docx

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import re
 
 from bs4 import BeautifulSoup, Tag
+from docx import Document
+from docx.shared import RGBColor
 
 
 def extract_values(file_path: str):
@@ -16,7 +18,7 @@ def extract_values(file_path: str):
             return {f'x{var}': int(coeff) for coeff, var in coefficients}
 
 
-def extract_tables_from_html(file_path: str):
+def extract_tables(file_path: str):
     with open(f"data/{file_path}", "r", encoding="utf-8") as file:
         soup = BeautifulSoup(file, 'html.parser')
         tables = soup.find_all("table", class_="table table-bordered table-center")
@@ -33,7 +35,7 @@ def extract_tables_from_html(file_path: str):
         return filtered_tables
 
 
-def transform_table(table, target_values):
+def transform_tables(table, target_values):
     # Добавляем строку сверху
     num_columns = len(table.find_all('tr')[0].find_all(['td', 'th']))
     header_row = Tag(name='tr')
@@ -67,18 +69,58 @@ def transform_table(table, target_values):
     return table
 
 
+def extract_result(file_path: str):
+    with open(f"data/{file_path}", "r", encoding="utf-8") as file:
+        soup = BeautifulSoup(file, 'html.parser')
+        text = soup.get_text()
+
+        optimal_plan_pattern = re.compile(
+            r"F\(X\)\s*=\s*[0-9*\/\+\-\s]+=\s*[0-9*\/]+",
+            re.DOTALL
+        )
+
+        match = optimal_plan_pattern.search(text)
+        if match:
+            return f"Результат: {match.group(0)}"
+        else:
+            return (f"Результат: Последняя строка содержит отрицательные элементы. "
+                    "Пространство допустимых решений неограниченно. Решения не существует")
+
+
+def save_table_to_docx(tables, output_file, result_text):
+    doc = Document()
+
+    for table in tables:
+        doc_table = doc.add_table(rows=0, cols=len(table.find_all('tr')[0].find_all(['td', 'th'])))
+
+        for row in table.find_all('tr'):
+            doc_row = doc_table.add_row().cells
+            for i, cell in enumerate(row.find_all('td')):
+                doc_row[i].text = cell.get_text().strip()
+                if cell.get('bgcolor'):
+                    run = doc_row[i].paragraphs[0].runs[0]
+                    run.font.color.rgb = RGBColor(255, 0, 0)
+                    run.bold = True
+
+    doc.add_paragraph(result_text)
+    doc.save(output_file)
+
+
 def parse():
     html_files = ["data1.html", "data2.html", "data3.html"]
-    output_dir = "extracted_tables"
+    output_dir = "extracted"
 
     for html_file in html_files:
         target_values = extract_values(html_file)
-        tables = extract_tables_from_html(html_file)
-        for i, table in enumerate(tables):
-            transformed_table = transform_table(table, target_values)
-            output_file_path = os.path.join(output_dir, f'{os.path.basename(html_file)}_table_{i + 1}.html')
-            with open(output_file_path, "w", encoding="utf-8") as file:
-                file.write(transformed_table.prettify())
+        tables = extract_tables(html_file)
+        result = extract_result(html_file)
+
+        transformed_tables = [transform_tables(table, target_values) for table in tables]
+        output_docx_path = os.path.join(
+            output_dir,
+            f'{os.path.basename(html_file).split(".")[0]}.docx'
+        )
+        save_table_to_docx(transformed_tables, output_docx_path, result)
 
 
 if __name__ == '__main__':
